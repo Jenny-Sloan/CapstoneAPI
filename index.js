b25a878fe17e529096ff10fc542aa889f204dd44
Jenny-Sloan <jennysloan83@gmail.com>
2019-08-27 15:53:53 -0600

    updated with notes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const app = express()
-const port = 4000
 const monk = require('monk')
+//middleware
 const cors = require('cors')
+//to prevent another domain to manipulate your data - for express but not built into it
 const bodyParser = require('body-parser')
+const app = express()
+//express is building our server - a library or framework to use to make your server
+const port = 4000
+
 
 // Connection URL
 const url = 'mongodb://JennySloan:duchess@bradscluster-shard-00-00-0v8rx.mongodb.net:27017,bradscluster-shard-00-01-0v8rx.mongodb.net:27017,bradscluster-shard-00-02-0v8rx.mongodb.net:27017/Blab?ssl=true&replicaSet=BradsCluster-shard-0&authSource=admin&retryWrites=true&w=majority';
@@ -17,13 +21,16 @@ const dogs = db.get('OH')
 
 app.use(cors())
 app.use(bodyParser.json())
+//runs with express
 
 app.get('/', async function (req, res) {
     const results = await dogs.find()
     res.status(200).send(results)
 })
+// '/' setting your end point for your server
 
 app.put('/:id', async function (req, res) {
+    //setting the end point as the variable of our required parameters of the ID
     console.log(req.body)
     const results = await dogs.findOneAndUpdate(req.params.id, {$set: req.body})
    
